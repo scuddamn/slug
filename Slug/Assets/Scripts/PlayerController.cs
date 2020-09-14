264f79586411be813e87507a576d86bd97ebebaa
scuddamn <aescudder@gmail.com>
2020-09-14 12:17:33 -0400

    made project + player controller

---
+++ Slug/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float force = 5;
+    Rigidbody2D rb;
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //RIGHT
+        //if <right> is pressed
+        if (Input.GetKey(right))
+        {
+            //apply a force using the "force" var
+            rb.AddForce(Vector2.right * force);
+        }
+
+        //LEFT
+        //if <left> is pressed
+        if (Input.GetKey(left))
+        {
+            //apply a force using the "force" var
+            rb.AddForce(Vector2.left * force);
+        }
+
+        //DOWN
+        //if <down> is pressed
+        if (Input.GetKey(down))
+        {
+            //apply a force using the "force" var
+            rb.AddForce(Vector2.down * force);
+        }
+
+        //UP
+        //if <up> is pressed
+        if (Input.GetKey(up))
+        {
+            //apply a force using the "force" var
+            rb.AddForce(Vector2.up * force);
+        }
+    }
+}
