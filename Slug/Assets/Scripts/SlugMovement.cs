fed8e2a4a5d7e2af77ef10d7d11f97033c0cd69b
scuddamn <aescudder@gmail.com>
2020-09-27 16:55:21 -0400

    slug is animated!!! floaty, sluggy, goodness!!

--- Slug/Assets/Scripts/SlugMovement.cs
+++ Slug/Assets/Scripts/SlugMovement.cs
@@ -7,6 +7,7 @@ public class SlugMovement : MonoBehaviour
     public float power = 10f;
     public Rigidbody rb;
 
+    Animator anim;
 
     public Vector3 minPower;
     public Vector3 maxPower;
@@ -22,6 +23,7 @@ public class SlugMovement : MonoBehaviour
     private void Start()
     {
         tl = GetComponent<TrajectoryLine>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -33,6 +35,7 @@ public class SlugMovement : MonoBehaviour
             
             Debug.Log(startPoint);
             rb.isKinematic = true;
+            anim.SetTrigger("MouseDown");
 
         }
 
@@ -49,6 +52,7 @@ public class SlugMovement : MonoBehaviour
             endPoint = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5)); //cam.nearClipPlane));
             
             rb.isKinematic = false;
+            anim.SetTrigger("MouseUp");
 
             force = new Vector3(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y), 0);
             rb.AddForce(force * power, ForceMode.Impulse);
